a4a47cff8c9a058842fd183b0fc59ac6aae4f4a6
Arnaud ALCABAS <aalcabas@scaleway.com>
2022-03-21 11:35:08 +0100

    first commit

---
+++ Formula/gocerts.rb
@@ -0,0 +1,17 @@
+class Gocerts < Formula
+  desc "Generate certificates and CA based on a yaml file"
+  homepage "https://github.com/n-Arno/gocerts"
+  url "https://github.com/n-Arno/gocerts/archive/refs/tags/v3.0.tar.gz"
+  sha256 "bbcfc1365ba5b0351a7f2ae98362433e768e0443865d33b8c6e7a956ca290bd4"
+  license "Unlicense"
+
+  depends_on "go@1.17" => :build
+
+  def install
+    system "go", "build", "-o", "#{bin}/gocerts"
+  end
+
+  test do
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+homebrew-thingies
+=================
+
+Personal tap of homebrew formulae, install using:
+
+```
+brew tap n-arno/thingies
+```
